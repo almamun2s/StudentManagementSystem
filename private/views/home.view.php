f7adff4a8bfa4b95048f83740228dd5950e99778
Abdullah Almamun <mamun2social@gmail.com>
2024-02-29 01:29:11 +0600

    some design changes

--- private/views/home.view.php
+++ private/views/home.view.php
@@ -1,11 +1,5 @@
 <?php $this->view('includes/header') ?>
     <div class="container-fluid">
-        <h1><i class="fab fa-facebook"></i> Home view</h1>
     </div>
-    <?php 
-        echo '<pre>';
-        var_dump($row);
-        echo '</pre>';
-    ?>
     
 <?php $this->view('includes/footer') ?>
--- private/views/includes/nav.view.php
+++ private/views/includes/nav.view.php
@@ -4,12 +4,10 @@
         <li><a href="#">Users</a></li>
         <li><a href="#">Classes</a></li>
         <li><a href="#" class="active" >Tests</a></li>
-        <li>User
-            <ul>
-                <li><a href="profile">Profile</a></li>
-                <!-- <li><a href="#">Hi</a></li> -->
-                <li><a href="logout">Logout</a></li>
-            </ul>
-        </li>
+        <!-- <li><a href="#">User</a></li> -->
+    </ul>
+    <ul>
+        <li><a href="profile">Profile</a></li>
+        <li><a href="logout">Logout</a></li>
     </ul>
 </nav>
