3f7e011052fdff0612f9c7e39c1a87bc4e75f8a2
Abdullah Almamun <mamun2social@gmail.com>
2024-03-02 02:27:26 +0600

    generated ids

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 require_once 'private/core/autoload.php';
 
 // echo 'Hello';
--- private/models/user.php
+++ private/models/user.php
@@ -77,15 +77,15 @@ class User extends Model{
 
 
     public function make_user_id($data){
-        // Logic will goes here
+        $data['user_id'] = $this->random_string(10);
         return $data;
-
     }
 
     public function make_school_id($data){
-        // Logic will goes here
+        if ( isset($_SESSION['USER']->school_id )) {
+            $data['school_id'] = $_SESSION['USER']->school_id;
+        }
         return $data;
-
     }
 
     public function make_hash_password($data){
@@ -97,5 +97,18 @@ class User extends Model{
     public function make_date($date){
         $date['date'] = date('Y-m-d h:i:s');
         return $date;
+    }
+
+    private function random_string(int $length){
+
+        $array = array( 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' );
+        $text = '';
+
+        for ($i=0; $i < $length; $i++) { 
+            $random = rand(0, 61);
+            $text .= $array[$random];
+        }
+
+        return $text;
     }
  }
