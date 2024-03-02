1437796dca424a0ad37e9761d66a1104f18ef3e4
Abdullah Almamun <mamun2social@gmail.com>
2024-03-03 01:48:42 +0600

    school view added

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,11 @@
-// alert('Hello');
+const addSchoolBtn      = document.getElementById('addNewSchool');
+const addSchoolCancel   = document.getElementById('addNewSchoolCancel');
+const addSchoolPopup    = document.querySelector('.sms-add_new_school');
+
+addSchoolBtn.addEventListener('click', function(){
+    addSchoolPopup.classList.add('sms-add_new_school_show');
+});
+
+addSchoolCancel.addEventListener('click', function(){
+    addSchoolPopup.classList.remove('sms-add_new_school_show');
+});
--- private/views/schools.view.php
+++ private/views/schools.view.php
@@ -3,7 +3,16 @@
         <?php $this->view('includes/b_crumb') ?>
 
         <div class="card-group justify-content-center">
-
+            <table class="table table-striped table-hover" >
+                <tr>
+                    <th>School</th>
+                    <th>Created By</th>
+                    <th>Date</th>
+                    <th>
+                        <span class="btn btn-primary" id="addNewSchool" ><i class="fas fa-plus"></i> Add new School</span>
+                    </th>
+                </tr>
+            </table>
             <?php if($schools) : ?>
                 <?php foreach ($schools as $school ): ?>
 
@@ -16,4 +25,16 @@
         </div>
     </div>
     
+    <!-- Add New School Popup  -->
+    <div class="sms-add_new_school">
+        <div class="sms-school_form border shadow p-4">
+            <form action="#" method="post">
+                <h3>Add new School</h3>
+                <input type="text" name="school" class="form-control" autofocus placeholder="School Name" >
+                <input type="submit" value="Create" class="btn btn-primary mt-4 float-end" >
+
+                <span class="btn btn-danger mt-4" id="addNewSchoolCancel" >Cancel</span>
+            </form>
+        </div>
+    </div>
 <?php $this->view('includes/footer') ?>
